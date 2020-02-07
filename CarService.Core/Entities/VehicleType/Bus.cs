f219fa74e5fd8382f767111e2644d93881273dfc
Nurk21 <oleksandr_voskolovych@ukr.net>
2020-02-07 15:55:08 +0200

    Add VehicleTypes

--- CarService.Core/Entities/VehicleType/Bus.cs
+++ CarService.Core/Entities/VehicleType/Bus.cs
@@ -1,22 +1,22 @@
 ﻿using CarService.Core.Entities.DetailsType;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarService.Core.Entities.VehicleType
 {
-    
-    public interface IBaseCar
+    public class Bus : IBaseCar
     {
-        [Key]
         public int Id { get; set; }
         public int BodyId { get; set; }
-        public Body Body { get; set; }    
+        public Body Body { get; set; }
         public int BrakesId { get; set; }
         public Brakes Brakes { get; set; }
         public int UndercarriageId { get; set; }
         public Undercarriage Undercarriage { get; set; }
-
+        public int HandrailId { get; set; }
+        public Handrail Handrail { get; set; }
+        public int CabinId { get; set; }
+        public Cabin Cabin { get; set; }
     }
 }
--- CarService.Core/Entities/VehicleType/Truck.cs
+++ CarService.Core/Entities/VehicleType/Truck.cs
@@ -1,22 +1,18 @@
 ﻿using CarService.Core.Entities.DetailsType;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarService.Core.Entities.VehicleType
 {
-    
-    public interface IBaseCar
+    public class Truck : IBaseCar
     {
-        [Key]
         public int Id { get; set; }
         public int BodyId { get; set; }
-        public Body Body { get; set; }    
+        public Body Body { get; set; }
         public int BrakesId { get; set; }
         public Brakes Brakes { get; set; }
         public int UndercarriageId { get; set; }
         public Undercarriage Undercarriage { get; set; }
-
     }
 }
